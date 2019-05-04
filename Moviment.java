7fbab9b4adf4e6e93753f4f155f81c424628715f
Khaedern <kaedern@gmail.com>
2019-05-04 23:56:20 +0200

    VersioDesenvolupament2

---
+++ Moviment.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+// Fitxer Moviment.java
+package Banc;
+
+import java.util.Date;
+
+public class Moviment
+{
+      private int numero;
+      private String descripcio;
+      private Date data;
+      private double quantitatDiners;
+}
