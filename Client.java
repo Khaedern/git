a5828a54282e8c8559f6dbab562c65c9ab2352ac
Khaedern <kaedern@gmail.com>
2019-05-04 23:24:56 +0200

    VersioDesenvolupament1

---
+++ Client.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+// Fitxer Client.java
+package Banc;
+
+public class Client {
+
+	private int codi;  
+	private String nom; 
+	private String cognoms;	   
+}
