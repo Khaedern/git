1e972cce3de354b1469675521b34ecd8ebd2fa3d
Khaedern <kaedern@gmail.com>
2019-05-04 23:56:36 +0200

    VersioDesenvolupament2

--- Client.java
+++ Client.java
@@ -4,11 +4,37 @@
  * and open the template in the editor.
  */
 // Fitxer Client.java
+// Fitxer Client.java
 package Banc;
 
 public class Client {
 
-	private int codi;  
-	private String nom; 
-	private String cognoms;	   
+    private int codi;
+    private String nom;
+    private String cognoms;
+
+    public int getCodi() {
+        return codi;
+    }
+
+    public void setCodi(int codi) {
+        this.codi = codi;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getCognoms() {
+        return cognoms;
+    }
+
+    public void setCognoms(String cognoms) {
+        this.cognoms = cognoms;
+    }
+
 }
